cb4e057b525b58eb0ec5399e4a6305f650d23c4f
DazzlingFame <Narsel1995@mail.ru>
2016-03-30 11:17:18 +0300

    последняя - рабочая
    делает, что нужно

--- Lab3pp/src/Mainpack/text_var4.java
+++ Lab3pp/src/Mainpack/text_var4.java
@@ -37,17 +37,7 @@ package Mainpack;
 			return builder.toString();
 		}
 
-		/*public static void task1(String str1, String str2) {
-			Pattern pattern = Pattern.compile(str1);
-			Matcher matcher = pattern.matcher(str2);
-			int i = 0;
-			while (matcher.find()) {
-				System.out.println("Found constans: " + matcher.group());
-				i++;
-			}
-			System.out.println("Count: "+ i);
-		}*/
-		
+
 		public static void  task2(String oldPattern, String newPattern, String read, String wright) throws FileNotFoundException {
 			Pattern pattern = Pattern.compile(oldPattern);
 			Matcher matcher = pattern.matcher(read);
@@ -71,20 +61,12 @@ package Mainpack;
 		
 		public static void main(String[] args) throws IOException {
 			//String readFile = "C:\\Users\\1\\Desktop\\text.txt";
-			String readFile = "C:\\Users\\1\\Desktop\\s.txt";
-			String wrightFile = "C:\\Users\\1\\Desktop\\file.txt";
+			String readFile = "H:\\промыш. прог\\lab3\\s.txt";
+			String wrightFile = "H:\\промыш. прог\\lab3\\file.txt";
 			String x = make(readFile);
 			//task2("(|)(\\w)(|)(-{0,1})(\\d+)(|)(\\+\\d+)(|)(\\w+)(@{0,1})(@)(\\w+)(.{0,1})(.)(\\w+)", "$1$2$3$5$6$7$8$10$11$12$14$15", x, wrightFile);
-			task2("(\\w+)(\\|)(-)(\\d+)(\\|)(\\+?)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\|)(\\w+)(@*)(@)(\\w+)", "$1$2$4$5$6$8\\ ($10$12$14)\\ $16$18$20-$22-$24-$26-$28$29$30$32$33", x, wrightFile);
-			//task1("\\b\\d{1,4}\\b", x);//количество числовых констант короче 4 символов
-			//task1("\\b(\\d{1,2}|[123]\\d{1,2}|[4][1234]\\d)\\b", x);//количество числовых констант со значением меньше «450»
-			//task1("\\b([1-9]{1,}[0]?[1-9]{0,}|[1-9]{1,}([^0])\\2)\\b", x);//количество числовых констант, неделимые без остатка на 100
-			//task1("\\b(\\d)\\d*\\1\\b", x);//количество числовых констант, в котором первая и последняя цифры одинаковые.
-			//task2("\\ +","\t", x, wrightFile);//заменяет один или несколько  идущих подряд пробелов символом табуляции
-			//task2("\\b(\\d+)(\\s|$)\\b", "$1\\0 ", x, wrightFile);
-			//task2("\\b(\\d+)(,)(\\d)(\\d{0,})([0])\\b", "$1$2$3$4 ",  make(wrightFile), wrightFile);
-			//task2("\\b(\\d+)(,)(\\d)\\b", "$1$3 ",  make(wrightFile), wrightFile);
-			//task2("\\b(\\d+)(,)(\\d)(\\d{1,})\\b", "$1$3$2$4 ",  make(wrightFile), wrightFile);
+			//task2("(\\w+)(\\|)(-)(\\d+)(\\|)(\\+?)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\|)(\\w+)(@*)(@)(\\w+)", "$1$2$4$5$6$8\\ ($10$12$14)\\ $16$18$20-$22-$24-$26-$28$29$30$32$33", x, wrightFile);
+			task2("([A-Z][a-z]*)(\\ ?)([A-Z][a-z]*)(\\|)(-)(\\d+)(\\|)(\\+?)(\\s*)(\\d)(\\s*)(\\(?)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\(?)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\s*)(\\d)(\\|)(\\w+)(@*)(@)(\\w+)", "$1\\ $3$4$6$7$8$10\\ ($14$16$18)\\ $22$24$26-$28-$30-$32-$34$35$36$38$39", x, wrightFile);
 		}
 		
 	}
